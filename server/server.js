941b0ae90e96ff8d8b8a237440d9fe97ce62d237
mic <mic83@arcor.de>
2017-09-26 23:06:16 +0200

    Add test cases for GET todos by id

--- server/server.js
+++ server/server.js
@@ -49,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
             if (!todo) {
                 return res.status(404).send();
             }
-            res.send(todo);
+            res.send({todo});
         }, () => res.status(400).send());
     }
 });
